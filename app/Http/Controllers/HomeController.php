d74b32d72be43af92b32ac84e6e8130e7052417d
Yeimag <marielaag.15@gmail.com>
2017-06-09 11:03:25 -0600

    Slide

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
 
 use Illuminate\Http\Request;
 use DB;
@@ -31,6 +32,27 @@ class HomeController extends Controller
 
         $cupones = DB::table('cupones')->paginate(6);
 
-        return view('home',['promociones'=> $promociones , 'cupones' => $cupones]);
+        //$cosas = $cupones->sortByDesc('cantVentas');
+        //$articulos = $cosas->paginate(5);
+       // $articulos = DB::table('cupones')->sortByDesc('cantVentas')->values()->all();
+
+        $articulos = DB::table('cupones')->orderBy('cantVentas', 'desc')->get();
+      
+
+        return view('home',['promociones'=> $promociones , 'cupones' => $cupones, 'articulos'=> $articulos]);
+    }
+
+
+    public function cuponesPopulares(){
+
+        $categoria = Cupon::all();
+        //$articulos = $categoria->articulos()->paginate(5);
+
+        $articulos = $categoria->sortByDesc('cantVentas');
+       // $ordenados = $articulos->sortByDesc('cantVentas')->paginate(5);
+
+       // $ordenados->values()->all();
+        //return view('home')->with("articulos",$articulos);
+        return view('home',['articulos'=> $articulos]);
     }
 }
